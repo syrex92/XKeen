03183eddccb3ccb3cf29706c4b1fc8271e0798d6
Syrex <syrex92@gmail.com>
2025-02-18 15:19:59 +0300

    Интеграция парсера в xkeen

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 opkg install tar bind-dig cron ipset xray
 curl -s -L https://github.com/Skrill0/XKeen/releases/latest/download/xkeen.tar --output xkeen.tar && tar -xvf xkeen.tar -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar
 xkeen -i
+mkdir /opt/etc/unblock
+curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/ublock/config --output /opt/etc/unblock/config
+curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/ublock/xkeen_parser.sh --output /opt/sbin/xkeen_parser
+curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/ublock/S24xray --output /opt/etc/init.d/S24xray
+chmod +x /opt/sbin/xkeen_parser
+echo "Настройте xray в папке /opt/etc/xray/configs и адреса для разблокировки в /opt/etc/unblock/config"
+echo "После настройки запустите парсер xkeen_parser"
---
+++ unblock/parser.sh
@@ -0,0 +1,50 @@
+﻿#!/bin/sh
+
+
+logger_msg() {
+  logger -s -t parser "$1"
+}
+
+logger_failure() {
+  logger_msg "Error: ${1}"
+  exit 1
+}
+
+CONFIG="/opt/etc/unblock/config"
+if [ -f "$CONFIG" ]; then
+  . "$CONFIG"
+else
+  logger_failure "Не удалось обнаружить файл \"config\"."
+fi
+
+for _tool in dig grep ip rm seq sleep; do
+  command -v "$_tool" >/dev/null 2>&1 || \
+  logger_failure "Для работы скрипта требуется \"${_tool}\"."
+done
+
+PIDFILE="${PIDFILE:-/tmp/parser.sh.pid}"
+[ -e "$PIDFILE" ] && logger_failure "Обнаружен файл \"${PIDFILE}\"."
+( echo $$ > "$PIDFILE" ) 2>/dev/null || logger_failure "Не удалось создать файл \"${PIDFILE}\"."
+trap 'rm -f "$PIDFILE"' EXIT
+trap 'exit 2' INT TERM QUIT HUP
+
+logger_msg "Удаляем файл и скачиваем новые диапазоны IP по кодам стран"
+rm $TEMPFILE
+cp $FILE $TEMPFILE
+for code in $COUNTRIES
+do
+  logger_msg "Скачиваем диапазоны для $code"
+  curl -s https://raw.githubusercontent.com/ipverse/rir-ip/master/country/$code/ipv4-aggregated.txt | grep -v '^#' >> $TEMPFILE
+done
+rm $TEMPFILE.sh
+cp $TEMPFILE $TEMPFILE.sh
+sed -i '/^#/d' $TEMPFILE.sh
+sed -i 's/^/ipset add ru /g' $TEMPFILE.sh
+sed -i '1i ipset create ru nethash' $TEMPFILE.sh
+sed -i '1i ipset flush ru' $TEMPFILE.sh
+chmod +x $TEMPFILE.sh
+$TEMPFILE.sh
+ipset add ru $COUNTRIES
+xkeen -start
+
+exit 0
