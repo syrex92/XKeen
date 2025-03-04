28ec9f31681edbb0f8530f20866586b3f8232f02
syrex92 <syrex92@gmail.com>
2025-03-04 17:49:32 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ curl -s -L https://github.com/Skrill0/XKeen/releases/latest/download/xkeen.tar -
 xkeen -i
 mkdir /opt/etc/unblock
 curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/unblock/config --output /opt/etc/unblock/config
-curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/unblock/xkeen_parser.sh --output /opt/sbin/xkeen_parser
+curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/unblock/parser.sh --output /opt/sbin/xkeen_parser
 curl -s -L https://raw.githubusercontent.com/syrex92/XKeen/main/unblock/S24xray --output /opt/etc/init.d/S24xray
 chmod +x /opt/sbin/xkeen_parser
 echo "Настройте xray в папке /opt/etc/xray/configs и адреса для разблокировки в /opt/etc/unblock/config"
